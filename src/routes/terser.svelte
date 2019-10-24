6a3e66dd042c7083e85a14c306b5340cd0eee0b7
Hadeeb Farhan <hadeebfarhan1@gmail.com>
2019-10-25 00:54:28 +0530

    basic terser setup

--- src/routes/terser.svelte
+++ src/routes/terser.svelte
@@ -1,5 +1,78 @@
+<script>
+  let code = "";
+  let minifiedCode = "";
+  $: {
+    if (window.Terser) {
+      const x = Terser.minify(code);
+      if (x.code) {
+        minifiedCode = x.code;
+      }
+    }
+  }
+
+  function onCopy() {
+    navigator.permissions
+      .query({ name: "clipboard-write" })
+      .then(({ state }) => {
+        if (state !== "granted") throw Error("Can't write to clipboard");
+      })
+      .then(() => {
+        navigator.clipboard.writeText(minifiedCode).then(() => {
+          console.log("Copied");
+        });
+      })
+      .catch(err => {
+        console.error(err);
+        if (err.message) alert(err.message);
+      });
+  }
+</script>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 80vh;
+  }
+  label {
+    flex: 1;
+    max-height: 48%;
+    display: flex;
+    flex-direction: column;
+  }
+  textarea {
+    flex: 1;
+    resize: none;
+  }
+
+  @media (min-width: 480px) {
+    main {
+      flex-direction: row;
+    }
+    label {
+      max-height: unset;
+      max-width: 48%;
+    }
+  }
+</style>
+
 <svelte:head>
   <title>Terser Online</title>
+  <script src="https://unpkg.com/terser@4">
+
+  </script>
 </svelte:head>
 
-<article>TODO</article>
+<main>
+  <label>
+    <span>Input</span>
+    <textarea bind:value={code} />
+  </label>
+  <label>
+    <span>Output</span>
+    <textarea disabled bind:value={minifiedCode} />
+  </label>
+</main>
+
+<button on:click={onCopy}>Copy</button>
